29e513016059448059f0c28c9e5c200cbc3e7ffa
Andrew Schmelyun <schmelyun@gmail.com>
2020-10-18 12:40:45 -0400

    Fixes #11, updates npm dependencies and swaps default watch for webpack-dev-server

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,10 @@ mix.disableNotifications();
 mix.webpackConfig({
     plugins: [
         build.cleaver
-    ]
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    }
 });
 
 mix.setPublicPath('./')
@@ -22,13 +25,15 @@ mix.setPublicPath('./')
 
 mix.browserSync({
     files: [
+        "dist/**/*",
         {
             match: ["resources/**/*"],
             fn: function(event, file) {
-                command.get('php cleaver', (error, stdout, stderr) => {
+                command.get('php cleaver build', (error, stdout, stderr) => {
                     console.log(error ? stderr : stdout);
                 });
             }
         }
-    ]
+    ],
+    proxy: 'localhost:8080'
 });
