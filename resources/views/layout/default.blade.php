2430e80c06eff68bc615354bd9b5703b76b236c2
Andrew Schmelyun <schmelyun@gmail.com>
2021-01-12 18:36:56 -0500

    Changes version in default view

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -31,7 +31,7 @@
     <div style="flex: 4">
         <main class="px-6 md:px-0">
             <header class="width-100 border-b-2 mb-8 pb-4">
-                <h1 class="text-3xl md:text-4xl font-thin text-gray-900 leading-snug">Welcome <span class="text-pink-600 text-sm font-semibold">V1.2.0</span></h1>
+                <h1 class="text-3xl md:text-4xl font-thin text-gray-900 leading-snug">Welcome <span class="text-pink-600 text-sm font-semibold">V1.5.5</span></h1>
                 <h3 class="text-base md:text-lg text-gray-700">A quick start guide for installing and using Cleaver.</h3>
             </header>
             @foreach($sections as $section)
